4efad673bdd8cfd2eb314c1207c65ae5813565ee
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2015-05-21 16:28:04 +0100

    Added a Callback for calculating the time entry and ensure that we can't invoice for more than ,000 at a time

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,11 +1,17 @@
 class TimeEntry < AccountEntry
   belongs_to :employee
 
+  before_create :calculate_amount!
+
   def self.per_hour
     34
   end
 
   def calculate_amount!
     self.amount = TimeEntry.per_hour * self.time
+    if self.amount > 1000
+    	false
+    end
+    true
   end
 end
